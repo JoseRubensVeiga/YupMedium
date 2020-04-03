7c10d906053cb85c7ec0d649fc04545c67e594d0
José <jose.rubens.veiga@gmail.com>
2020-04-03 13:57:46 -0300

    finish project

--- app.js
+++
@@ -1,7 +0,0 @@
-function getNumberWithDecimals(number, decimals = 2) {
-  return number.toFixed(decimals);
-}
-
-getNumberWithDecimals(10, 3); // 10.000
-
-getNumberWithDecimals("10", 3); // number.toFixed is not a function
